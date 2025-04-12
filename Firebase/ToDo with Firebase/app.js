9da917e623dbdc8649cc4627f758bb1d548d1e6d
mzaidrizwan <z0887451@gmail.com>
2025-04-12 14:22:37 +0500

    ADD,DELETE,EDIT updated Program using FIREBASE

--- Firebase/ToDo with Firebase/app.js
+++ Firebase/ToDo with Firebase/app.js
@@ -1,45 +1,67 @@
-import { initializeApp } from "";//authentication link here in quotations --> ""
-import { getFirestore, collection, getDocs, query, orderBy, addDoc, serverTimestamp } from "";//authentication link here in "",at last of link type -firestore.js
+import { initializeApp } from "";
+import { getFirestore, collection, getDocs, query, orderBy, addDoc, serverTimestamp, doc, updateDoc, deleteField, deleteDoc, onSnapshot } from "";
 
 const ul = document.getElementById("ul");
 
+
 const firebaseConfig = {
-//Configuration keys from your Firebase account
-    apiKey: "AIzaSyB6RhEK433z3cPfXA1PnBKCKkrVb-2i9ug",
-    authDomain: "database-basic-33625.firebaseapp.com",
-    projectId: "database-basic-33625",
-    storageBucket: "database-basic-33625.firebasestorage.app",
-    messagingSenderId: "591813824225",
-    appId: "1:591813824225:web:a10d12a72f7f821b7a71a0",
-    measurementId: "G-9MMTLVTCY4"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const todoCollectionRef = collection(db, "todo");
+const q = query(todoCollectionRef, orderBy("time")); // You can add 'desc' for descending order
+
+onSnapshot(q, (querySnapshot) => {
+    ul.innerHTML = "";
+
+    querySnapshot.forEach((doc) => {
+        ul.innerHTML += `<li>${doc.data().item} 
+            <button onclick="deletItem('${doc.id}')">DELETE</button> 
+            <button onclick="updateItem('${doc.id}')" >EDIT</button>
+        </li>`;
+    });
+}, (error) => {
+    console.error("Error fetching realtime data: ", error);
+});
 
 document.getElementById("btn").addEventListener("click", async () => {
-    let inp = document.querySelector("input[name='inp']");
+    const inp = document.querySelector("input[name='inp']");
     try {
-        const docRef = await addDoc(collection(db, "todo"), {
+        const docRef = await addDoc(todoCollectionRef, {
             item: inp.value,
-            time: serverTimestamp() 
+            time: serverTimestamp()
         });
         console.log("Document written with ID: ", docRef.id);
     } catch (e) {
-        console.error("Error adding document: ", e);
+        console.log("Error adding document: ", e);
     }
-    
-    callData();
+    inp.value = "";
 });
 
-async function callData() {
-    ul.innerHTML = "";
-    const q = query(collection(db, "todo"), orderBy("time")); //AGAR DECENDING ORDER ME ITEM LISTING CHAHYE TO:orderBy("time", "desc") LIKHEN GY. 
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-        // console.log(doc.data()); 
-        ul.innerHTML += `<li>${doc.data().item}</li>`;
+async function deletItem(e) {
+    const cityRef = doc(db, "todo", e);
+    await deleteDoc(cityRef, {
+        item: deleteField(),
+        time: deleteField()
     });
-}callData();
+}
+window.deletItem = deletItem;
 
+async function updateItem(e) {
+    let newInp = prompt("Enter new text for the item:");
+    if (!newInp) return;
 
+    const cityRef = doc(db, "todo", e);
+    await updateDoc(cityRef, {
+        item: newInp
+    });
+}
+window.updateItem = updateItem;
