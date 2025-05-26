c399346893e4de697e04079a9b5bf3761599fa0f
mzaidrizwan <z0887451@gmail.com>
2025-05-26 20:43:19 +0500

    extracting data from given input

--- C++/String/extractINSIGHTSfromGIVENinfo.cpp
+++ C++/String/extractINSIGHTSfromGIVENinfo.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     char info[50] = "barac 1 sevilla 2,madric 0 barac 0 madric 2";
-    
+
     char *info_Ptr = info;
     int info_size = 0;
     int jump_detect = 2;
@@ -18,13 +18,13 @@ int main()
 
     while (*(info_Ptr++) != '\0')
     {
-        if ((info[i] >= 97 && info[i] <= 122) || (info[i] >= 65 && info[i] <= 90) )
+        if ((info[i] >= 97 && info[i] <= 122) || (info[i] >= 65 && info[i] <= 90))
         {
             element += info[i];
         }
         else if (info[i] >= 48 && info[i] <= 57)
         {
-         eachTeamscore.push_back(info[i] - 48);   
+            eachTeamscore.push_back(info[i] - 48);
         }
         else
         {
@@ -41,22 +41,28 @@ int main()
             eachTeamName.push_back(element);
         i++;
     }
-   
+
     cout << " Result = ";
-    for (size_t i = 0; i < eachTeamName.size(); i++)
-    {
-        cout << eachTeamName[i] << " ";
-    }
 
-    cout << " \n\nResult = ";
-    for (size_t i = 0; i < eachTeamscore.size(); i++)
-    {
-        cout << eachTeamscore[i] << " ";
-    }
 
+    int eachTeamName_SIZE =eachTeamName.size();
+
+    for (size_t i = 0; i < eachTeamName_SIZE - 1; i++)
+    {
+        for (size_t j = 1; j < eachTeamName.size(); j++)
+        {
+            if (eachTeamName[i] == eachTeamName[j])
+            {
+              cout << "team: "<< eachTeamName[i]<<" score: "<<eachTeamscore[i] + eachTeamscore[j]<<endl;
+            }
+            
+        }
+        
+    }
     
 
 
+    // cout << (eachTeamName[0] == eachTeamName[2 + 1]) << " ";
 
     return 0;
 }
