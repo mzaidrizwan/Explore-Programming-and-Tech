791334cfcfad090b4c113bda7a3c0344ba263800
mzaidrizwan <z0887451@gmail.com>
2025-05-27 11:34:10 +0500

    password strength measuring and making sure the rules of making a password are followed

--- C++/String/passwordSTRENGTHvalidator.cpp
+++ C++/String/passwordSTRENGTHvalidator.cpp
@@ -7,33 +7,52 @@ int main()
     int length = 10;
     int letters = 2;
     int digits = 2;
-    string password = "9ahnvdbfgger67";
+    int finish_task = 1;
+    string password = "9ahnvdbFGger1";
     int i = 0;
 
     while (password[i] ? true : false)
     {
-        if (!((password[i] >= 97 && password[i] <= 122) || (password[i] >= 65 && password[i] <= 90) || (password[i] >= 48 && password[i] <= 57)))
+        if ((password[i] >= 97 && password[i] <= 122) || (password[i] >= 65 && password[i] <= 90))
+        {
+            --letters;
+        }
+        else if ((password[i] >= 48 && password[i] <= 57))
+        {
+            --digits;
+        }
+        else
         {
             answer();
+            finish_task = 0;
             break;
         }
 
         --length;
         ++i;
     }
-    if (length <= 0)
-    {
-        answer("Valid password");
-    }
-    if(length > 0)
-    {
-        answer();
-    }
 
+    if (finish_task)
+    {
+
+        if (length <= 0 && letters <= 0 && digits <= 0)
+        {
+            answer("Valid password");
+        }
+        else
+        {
+            answer();
+        }
+
+        if (length > 0)
+        {
+            answer();
+        }
+    }
     return 0;
 }
 
-void answer(string e )
+void answer(string e)
 {
     cout << e;
 }
