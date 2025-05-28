3320f9143305d8a9fb09be69ae97aa5a5e359da3
mzaidrizwan <z0887451@gmail.com>
2025-05-28 23:06:07 +0500

    anagram validator.New feature:Auto arranging all the elements

--- C++/String/anagramVALIDATOR.cpp
+++ C++/String/anagramVALIDATOR.cpp
@@ -6,7 +6,10 @@ int main()
     string a = "electION rESULTs";
     string b = "liEs lETs recOunt";
     string inp1, inp2;
-    int j,m = 0;
+    int size_b=0;
+    int size_a = 0;
+    int j=0;
+int m = 0;
     bool yes = true;
 
     for (size_t i = 0; a[i] != '\0'; i++)
@@ -15,11 +18,13 @@ int main()
         {
             inp1[j] = a[i] + 32;
             ++j;
+            ++size_a;
         }
         else if (a[i] >= 97 && a[i] <= 122)
         {
             inp1[j] = a[i];
             ++j;
+            ++size_a;
         }
     }
 
@@ -29,50 +34,50 @@ int main()
         {
             inp2[m] = b[i] + 32;
             ++m;
+            ++size_b;
         }
         else if (b[i] >= 97 && b[i] <= 122)
         {
             inp2[m] = b[i];
             ++m;
+            ++size_b;
         }
     }
 
-    cout << "\nAnswer : ";
-    for (size_t i = 0; inp2[i] != '\0'; i++)
+    if (size_a == size_b)
     {
-        /* code */
-        cout << inp2[i];
+
+        for (size_t i = 0; inp1[i] ? true : false; i++)
+        {
+            bool valid = false;
+
+            for (size_t k = 0; inp1[k] ? true : false; k++)
+            {
+                if (inp2[k] != '-' && inp1[i] == inp2[k])
+                {
+                    valid = true;
+                    inp2[k] = '-';
+                    break;
+                }
+            }
+
+            if (!valid)
+            {
+                cout << "\nNot valid :-(";
+                yes = false;
+                break;
+            }
+        }
+
+        if (yes)
+        {
+            cout << "\nvalid :-)";
+        }
     }
-
-
-    // for (size_t i = 0; inp1[i] ? true : false; i++)
-    // {
-    //     bool valid = false;
-
-    //     for (size_t k = 0; inp1[k] ? true : false; k++)
-    //     {
-    //         // cout << " 2 " << inp2[k] << endl<< " 1 "<<inp1[i];
-    //         if (inp2[k] != '-' && inp1[i] == inp2[k])
-    //         {
-    //             valid = true;
-    //             inp2[k] = '-';
-    //             break;
-    //         }
-    //     }
-
-    //     if (!valid)
-    //     {
-    //         cout << "\nNot valid :-(";
-    //         yes = false;
-    //         break;
-    //     }
-    // }
-
-    // // (valid ? "Yes" : "No")
-    // if (yes)
-    // {
-    //     cout << "\nvalid :-)";
-    // }
+    else
+    {
+        cout << "\nNot valid :-(";
+    }
 
     return 0;
 }
