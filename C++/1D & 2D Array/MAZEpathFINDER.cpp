b875d65c426447231c0468ec9dd8c4cb3530aa5b
mzaidrizwan <z0887451@gmail.com>
2025-06-18 17:00:59 +0500

    path finding algorithm

--- C++/1D & 2D Array/MAZEpathFINDER.cpp
+++ C++/1D & 2D Array/MAZEpathFINDER.cpp
@@ -1,125 +1,112 @@
 #include <iostream>
 using namespace std;
 
-bool should_initialize_again;
-int confused;
-void ask_if_Confused();
 int main()
 {
 
     char arr[5][5] = {
-        {'#', '#', ' ', '#', ' '},
-        {'#', '#', ' ', '#', ' '},
-        {'#', ' ', '#', ' ', '#'},
-        {'#', ' ', ' ', ' ', '#'},
+        {'#', ' ', '#', ' ', ' '},
+        {'#', ' ', ' ', ' ', ' '},
+        {'#', '#', '#', '#', ' '},
+        {'#', ' ', ' ', ' ', ' '},
         {'#', '#', ' ', '#', '#'}};
 
     int rows = sizeof(arr) / sizeof(arr[0]) - 1;
     int cols = sizeof(arr[0]) / sizeof(arr[0][0]) - 1;
     int i = 0, j;
-
-    cout << "rows" << rows << endl
-         << "cols" << cols << endl;
-    cout << endl;
-
-    cout << "j = " << j << endl;
+    bool isStartingPointAvailable = false;
+    char previousStep;
 
     while (true)
     {
-        should_initialize_again = true;
 
-        if (!should_initialize_again)
+        if (arr[i][j] == ' ')
         {
 
-            if (arr[i][j] == ' ')
-            {
-                ask_if_Confused();
-                arr[i][j] = '+';
+            previousStep = arr[i][j];
+            arr[i][j] = '!';
 
-                if (i == rows)
-                {
-                    break;
-                }
+            if (i == rows)
+            {
+                break;
+            }
+        }
+        else
+        {
+            if (arr[i + 1][j] == ' ' && i + 1 <= rows)
+            {
+
+                ++i;
+            }
+            else if (arr[i][j + 1] == ' ' && j + 1 <= cols)
+            {
+
+                j++;
+            }
+            else if (arr[i][j - 1] == ' ' && j - 1 >= 0)
+            {
+
+                j--;
+            }
+            else if (arr[i - 1][j] == ' ' && i - 1 >= 0)
+            {
+
+                i--;
             }
             else
             {
-                if (arr[i][j + 1] == ' ' && j + 1 <= cols)
+
+                if (arr[i][j + 1] == '!' && j + 1 <= cols)
                 {
-                    ask_if_Confused();
+
+                    arr[i][j] = '^';
                     j++;
                 }
-                else if (arr[i + 1][j] == ' ' && i + 1 <= rows)
+                else if (arr[i + 1][j] == '!' && i + 1 <= rows)
                 {
-                    ask_if_Confused();
+
+                    arr[i][j] = '^';
                     ++i;
                 }
-                else if (arr[i][j - 1] == ' ' && j - 1 >= 0)
+                else if (arr[i][j - 1] == '!' && j - 1 >= 0)
                 {
-                    ask_if_Confused();
+
+                    arr[i][j] = '^';
                     j--;
                 }
-                else if (arr[i - 1][j] == ' ' && i - 1 >= 0)
+                else if (arr[i - 1][j] == '!' && i - 1 >= 0)
                 {
-                    ask_if_Confused();
+
+                    arr[i][j] = '^';
                     i--;
                 }
                 else
                 {
-                    ask_if_Confused();
 
-                    if (arr[i][j + 1] == '+' && j + 1 <= cols)
+                    arr[i][j] = '^';
+                    for (size_t m = 0; m <= cols; m++)
                     {
-                        ask_if_Confused();
-                        arr[i][j] = '^';
-                        j++;
+                        if (arr[0][m] == ' ')
+                        {
+                            j = m;
+                            isStartingPointAvailable = true;
+                            break;
+                        }
                     }
-                    else if (arr[i + 1][j] == '+' && i + 1 <= rows)
+                    if (!isStartingPointAvailable)
                     {
-                        ask_if_Confused();
-                        arr[i][j] = '^';
-                        ++i;
-                    }
-                    else if (arr[i][j - 1] == '+' && j - 1 >= 0)
-                    {
-                        ask_if_Confused();
-                        arr[i][j] = '^';
-                        j--;
-                    }
-                    else if (arr[i - 1][j] == '+' && i - 1 >= 0)
-                    {
-                        ask_if_Confused();
-                        arr[i][j] = '^';
-                        i--;
+                        cout << "no valid point available to start.";
+                        isStartingPointAvailable = false;
+                        break;
                     }
                 }
-            }
-        }
-
-        if (should_initialize_again)
-        {
-            bool isStartingPointAvailable = false;
-
-            for (size_t m = 0; m <= cols; m++)
-            {
-
-                if (arr[i][m] == ' ')
-                {
-                    j = m;
-                    isStartingPointAvailable = true;
-                    break;
-                }
-            }
-            should_initialize_again = false;
-            if (!isStartingPointAvailable)
-            {
-                cout << "no valid point available to start.";
-                break;
             }
         }
     }
     // =========================[SHOW OUTPUT]=====================
     for (size_t m = 0; m <= rows; m++)
     {
+        cout << "|";
         for (size_t k = 0; k <= cols; k++)
         {
             if (arr[m][k] == '^')
@@ -129,11 +116,6 @@ int main()
 
             cout << arr[m][k] << " ";
         }
-        cout << endl;
+        cout << "|" << endl;
     }
-}
-
-void ask_if_Confused()
-{
-    should_initialize_again = false;
 }
